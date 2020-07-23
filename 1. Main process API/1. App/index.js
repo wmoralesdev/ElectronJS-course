f6c10f456ed8094bd670cdbb5830f1bcccd993c3
Walter26 <walterrafael26@gmail.com>
2020-07-23 12:14:52 -0700

    Initialize repo

---
+++ 1. Main process API/1. App/index.js
@@ -0,0 +1,21 @@
+// require with destructuring approach
+
+// create function to display window
+
+// listen on app 'ready' event
+
+/*
+    platform (OS) win32, darwin, linux
+    app.on
+    app.once
+
+        triggering events
+            before-quit
+            window-all-closed
+    
+    isReady
+    whenReady
+    focus
+    show
+    hide
+*/
